bdbc42ff53fcd5486abb0f058d4829911708fd82
Marco Tulio R Braga <marco.tulio@mtulio.eng.br>
2015-08-03 15:32:55 -0300

    Updating Makefile of Test application

---
+++ apps/mtapp01/Readme.md
@@ -0,0 +1,1 @@
+## Dummy program to test cross-compile
---
+++ apps/mtapp01/mtapp01.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/* Show Function name */
+int main(void) {
+	printf ("__FUNCTION__ = %s\n", __FUNCTION__);
+	
+	exit(0);
+}
