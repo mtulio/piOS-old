19889e70ecd1bcf65e94db594ab81f91c1ec536f
Marco Tulio R Braga <marco.tulio@mtulio.eng.br>
2015-08-03 15:38:20 -0300

    Pack cross compile and add script to install it - in development

---
+++ build/install_toolchain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# Check if toolchain is in the system, if isn't present install it.
+# '-> Might to be run as root
+# '-> Check arch
+# '-> Create a Sym Link in /tools directory required to build Linux Kernel
+# '-> Create config file
+#
+
+#
+# Created by: Marco Tulio R Braga (https://github.com/mtulio)
+#
